0a3f14cb10ceabb3211a7590f226cbe13889c294
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-09-25 16:04:17 +0100

    Add ability to delete an item.
    Add event listener to 'list' that checks if element
    has 'delete' class, if so, delete parent element.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+"use strict";
+
 function select(el) {
   return document.querySelector(el);
 }
 
 const list = select("ul");
 const input = select("input");
+const addButton = select("button.add-item");
 const button = select("button");
-console.log(button, input, list);
 
-button.addEventListener("click", function () {
+addButton.addEventListener("click", function () {
   const item = input.value;
   input.value = "";
   const listItem = document.createElement("li");
@@ -16,6 +18,12 @@ button.addEventListener("click", function () {
   listItem.appendChild(listSpan);
   listItem.appendChild(listButton);
   listSpan.textContent = item;
+  listButton.classList.add("delete");
   listButton.textContent = "Delete";
   list.appendChild(listItem);
+});
+list.addEventListener("click", function (e) {
+  e.target.classList.contains("delete")
+    ? e.target.parentElement.remove()
+    : null;
 });
